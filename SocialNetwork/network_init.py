e22288987fdafc3298121aa4619c46e4c91680d7
marcizzz <marcello.agazzi@mail.polimi.it>
2021-04-23 16:20:49 +0200

    minor fixes in network creation

--- SocialNetwork/network_init.py
+++ SocialNetwork/network_init.py
@@ -3,19 +3,30 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import json
 import numpy as np
+import random
 from ast import literal_eval
 
 class Network_creator:
 
     #Creates Barabasi-Albert Network with N nodes and M initial edges for each node
-    def __init__(self, nodes = 50, edges = 5, graph = None):
+    def __init__(self, nodes = 50, start_edges = 5, adj_matrix = None):
         self.nodes = nodes
-        self.edges = edges
+        self.start_edges = start_edges
 
-        if graph == None:
-            self.graph = nx.barabasi_albert_graph(n = nodes, m = edges)
+        #clear creation
+        if adj_matrix.any():
+            self.graph = nx.barabasi_albert_graph(n = nodes, m = start_edges)
+            self.categories = np.empty(shape = self.nodes)
+            self.adj_matrix = nx.adjacency_matrix(self.graph)
+
+            for i in range(self.nodes):
+                self.categories[i] = random.randint(1,5)
+
+        #needed for fromFilename purposes
         else:
-            self.graph = graph
+            self.adj_matrix = adj_matrix
+            self.graph = nx.from_numpy_matrix(adj_matrix)
+     
     
     #Generates a Network from adjacency matrix in specified file
     @classmethod
@@ -27,13 +38,15 @@ class Network_creator:
                     print("File is empty, creating a network with default parameters...")
                     return cls()
                 
-                adj_matrix_string = json.load(network_file)
-                adj_matrix = np.array(literal_eval(adj_matrix_string))
+                net_dict = json.load(network_file)
+
+                net = cls(nodes = net_dict["nodes"], 
+                          start_edges = net_dict["start_edges"],
+                          adj_matrix = np.array(net_dict["adj_matrix"]))
+
+                net.categories = net_dict["categories"]
                 
-                graph = nx.from_numpy_matrix(adj_matrix)
-                nodes,_ = adj_matrix.shape
-                
-                return cls(nodes = nodes, edges = _, graph = graph)
+                return net
         except FileNotFoundError:
             print("File not found, creating a network with default parameters...")
             return cls()
@@ -51,17 +64,21 @@ class Network_creator:
                 }
         
         #nx.draw_networkx(self.graph, arrows=False, with_labels=labels, **options)
-        nx.draw_random(self.graph, with_labels = True)
+        nx.draw_random(self.graph, with_labels = labels)
         plt.show()
     
     #Creates JSON file storing the adjacency matrix of the Network
     def generate_json(self):
-        adj_mat = nx.adjacency_matrix(self.graph).toarray()
         file_path = "SocialNetwork/network.json"
         network_file = open(file_path, 'w', encoding='utf-8')
-        
-        json_string = json.dumps(adj_mat.tolist())
-        json.dump(json_string, network_file, separators=(',',':'))
+
+        net_dict = {"nodes": self.nodes,
+                    "start_edges": self.start_edges,
+                    "categories": self.categories.tolist(),
+                    "adj_matrix": self.adj_matrix.tolist()
+                    }
+       
+        json.dump(net_dict, network_file, separators=(',',':'))
 
         network_file.close()
 
