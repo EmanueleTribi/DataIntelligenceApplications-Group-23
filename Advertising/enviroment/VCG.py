261538603c316c75edb7db5feee4a07068dcad53
EmanueleTribi <emanuele.tribi@mail.polimi.it>
2021-05-06 15:24:34 +0200

    tested vcg

--- Advertising/enviroment/VCG.py
+++ Advertising/enviroment/VCG.py
@@ -6,11 +6,47 @@ import operator
 deltas=[0.8,0.5,.44,0.40,0.35,0.20]
 
 #this is the best possible auction we can make 
+# test probability of observing a slot: equivalent to delta_s(observe prob) q_a(click probability)
+#i assume that the quality of the ad is for all 1 (WRONG ASSUMPTION!!!!)
+deltas=[0.8,0.5,.44,0.40,0.35,0.20]
+
+#this is the best possible auction we can make 
 
 class VCG():
     def __init__(self,deltas=[0.8,0.5,.44,0.40,0.35,0.20]):
         self.deltas=np.array(deltas)
 
+    
+
+## function that finds the best allocation  of 6 ads of a campaign
+
+    def best_allocation(self, bids):
+        #make a copy 
+        ausiliary_array = bids
+
+        best_allocation = []
+
+        for i in range(0, 6):
+
+            ##TODO: CHECK THE FUNCTION WITH MAX OPERATOR
+            
+            item = max(ausiliary_array, key=lambda item:item.bid)
+            best_allocation.append(item)
+            ausiliary_array.remove(item)
+
+        return best_allocation
+        
+    ##function that finds the best allocation of all the campaigns
+    def all_best_allocations(self, list_camp_bids):
+
+        best_alloc_all_camp = []
+    
+        for i in range(0, 5):
+            best_alloc_all_camp.append(self.best_allocation(list_camp_bids[i]))
+        
+        return best_alloc_all_camp
+
+'''
 ##this is the payment function 
 ## take in input the payment for a single ad (the bid)
 # and the list of all bids, necessary to calculate the VCG payment function
@@ -27,51 +63,4 @@ class VCG():
 
         payment=(xa-ya)/deltas[best_allo.index(bid)]
         return payment
-
-## function that finds the best allocation  of 6 ads of a campaign
-
-    def best_allocation(self,bids):
-        #make a copy 
-        ausiliary_array = bids
-
-        best_allocation = []
-
-        for i in range(0, 5):
-
-            ##TODO: CHECK THE FUNCTION WITH MAX OPERATOR
-            
-            item = max(ausiliary_array, key=operator.attrgetter('ad_id'))[0]
-            best_allocation.append(item)
-            ausiliary_array.remove(item)
-
-        return best_allocation
-        
-    ##function that finds the best allocation of all the campaigns
-    def all_best_allocations(self, list_camp_bids):
-
-        best_alloc_all_camp = []
-    
-        for i in range(0, 4):
-            best_alloc_all_camp.append(best_allocation(list_camp_bids(i)))
-        
-        return best_alloc_all_camp
-
-
-
-
-
 '''
-        combos= list(permutations(bids, 6))
-        values=[]
-        for combo in combos:
-           
-            bidsOfCombo=[]
-            for i in combo:
-                bidsOfCombo.append(i.bid)
-            bidsOfCombo=np.array(bidsOfCombo)    
-            values.append(np.inner(combo,self.deltas))
-        
-        return combos[np.argmax(values)]       
-'''   
-
-
