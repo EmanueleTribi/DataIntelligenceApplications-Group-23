f9a046a37bc7fd52b3cdbf102399b1037bef491f
Matteo Mainardi Salomoni <matteo1.mainardi@mail.polimi.it>
2021-08-31 16:40:54 +0200

    test point 4

--- Advertising/learners/TSLearner.py
+++ Advertising/learners/TSLearner.py
@@ -1,6 +1,7 @@
 import numpy as np
 from Advertising.enviroment.bid import Bid
 
+
 class TS_Learner:
     def __init__(self, n_arms, arms, n_categories, ad_id):
         self.ad_id = ad_id
@@ -15,26 +16,35 @@ class TS_Learner:
     def update_observations(self, pulled_arm, reward):
         self.collected_rewards.append(reward)
 
-  # select the arm to pull
+
     def pull_arm(self):
-        pulled_arm = []
-        for i in range(self.n_categories):
-            values = np.random.beta(self.beta_parameters[i, :, 0], self.beta_parameters[i, :, 1])
-            value_bid = np.argmax(values)
-            bid = Bid(value_bid, self.ad_id)
-            pulled_arm.append(bid)
-        return pulled_arm
+        bid=[]
+        values = np.random.beta(
+        self.beta_parameters[:, :, 0], self.beta_parameters[:, :, 1])
+        value_bid = np.argmax(values,axis=-1)
+        for i in range(len(value_bid)):
+            bid.append(Bid(value_bid[i], self.ad_id))
+        return bid
+
+  # select the arm to pull
+    # def pull_arm(self):
+    #   pulled_arm = []
+    # for i in range(self.n_categories):
+    #     values = np.random.beta(self.beta_parameters[i, :, 0], self.beta_parameters[i, :, 1])
+    #     value_bid = np.argmax(values)
+    #    bid = Bid(value_bid, self.ad_id)
+    #   pulled_arm.append(bid)
+    # return pulled_arm
 
     def update(self, pulled_arm, reward):
         self.t += 1
         self.update_observations(pulled_arm, reward)
+        new=[]
         for i in range(self.n_categories):
-            if(reward[i] >= self.prev_reward[i]):
-                temp = 1
-            else:
-                temp = 0
-            self.beta_parameters[i, pulled_arm[i].bid, 0] += temp
-            self.beta_parameters[i, pulled_arm[i].bid, 1] += 1.0 - temp
-        self.prev_reward = reward
-
-
+            if(self.prev_reward[i]<=reward[i]):
+                rew=1.0
+            else: rew=0.0
+            
+            self.beta_parameters[i, pulled_arm[i].bid, 0] += rew
+            self.beta_parameters[i, pulled_arm[i].bid, 1] += 1.0 - rew
+            self.prev_reward[i] = reward[i]
